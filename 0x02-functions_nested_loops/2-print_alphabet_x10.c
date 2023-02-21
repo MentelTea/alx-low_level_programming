e9907527d616345b0acf90489e3218349b6af483
Menteltea <janahhamza@hotmail.com>
2023-02-21 14:52:24 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ *main - entry point
+ *Description - prints 10 the alphabet times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char(c <= 'z')
+		{
+			while(c <= z)
+			{
+				_putchar(c);
+				c++;
+			}
+			_putchar('\n');
+					
+
+
+}
