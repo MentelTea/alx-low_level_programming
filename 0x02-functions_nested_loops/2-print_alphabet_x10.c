3de048b160b31b5dc3205a7fd52637b88c0758c7
Menteltea <janahhamza@hotmail.com>
2023-02-21 14:59:53 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,12 @@
 /*
  *main - entry point
  *Description - prints 10 the alphabet times
- *
+ *Return: void
  */
 void print_alphabet_x10(void)
 {
+	char c;
+	c = 'a';
 	char(c <= 'z')
 		{
 			while (c <= z)
